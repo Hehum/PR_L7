87b19828850f3c3728a00bfac89c169ee939df3b
Hehum <jarekkotowicz1@gmail.com>
2021-04-30 13:40:53 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Route, Switch, Redirect } from "react-router-dom";
 import './App.css';
 import Posts from "./components/posts";
+import NotFound from "./components/notFound";
+import Home from "./components/home";
+import NavBar from "./components/common/navbar";
 
 function App() {
  return (
-     <main className="container">
-         <Posts/>
-     </main>
+     <div className="container-fluid">
+            <NavBar />
+            <div className="container">
+                <div className="content">
+                    <Switch>
+                        <Route path="/posts"
+                        component={Posts} />}
+                        />
+                        <Route path="/not-found" component={NotFound} />
+                        <Route path="/" exact component={Home} />
+                        <Redirect to="/not-found" />
+                    </Switch>
+                </div>
+            </div>
+          </div>
+
  );
 }
 
